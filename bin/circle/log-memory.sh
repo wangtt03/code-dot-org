ca96e331a42d4333189ca786d78f3aef7858e5e7
Ram Kandasamy <ram@code.org>
2017-06-08 15:16:49 -0700

    Pull memory logging into a script

---
+++ bin/circle/log-memory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILE="${CIRCLE_ARTIFACTS:-.}/memory-usage-actual.txt"
+echo "Logging to $FILE..."
+TIMEFMT='[%Y-%m-%d %H:%M:%S]'
+free | ts "$TIMEFMT" | head -n 2 > $FILE;
+while true; do
+  sleep 10;
+  free | ts "$TIMEFMT" | sed -n 2p >> $FILE;
+done
